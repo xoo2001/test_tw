72249f29af273f934de8a35b989b7d5669cc23c7
Carlo Dee <carlodee.official@proton.me>
2024-08-30 01:56:36 +0000

    CK8n: Rewrite and sort boardconfig file.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_BOOT_HEADER_VERSION := 3
 # Switch to --vendor_cmdline since header v3
 BOARD_VENDOR_CMDLINE := bootopt=64S3,32N2,64N2
 # From 2048, switch to 4096 since header v3
-BOARD_KERNEL_PAGESIZE := 4096
+BOARD_PAGE_SIZE := 4096
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x11088000
@@ -62,9 +62,14 @@ BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 
+# Board - type/size ?
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_USES_SYSTEM_DLKMIMAGE := true
+
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := TECNO-CK8n
+TARGET_BOOTLOADER_BOARD_NAME := CK8n
 TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
 
 # Build Rules
 BUILD_BROKEN_DUP_RULES := true
@@ -89,7 +94,10 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
 # Kernel
 TARGET_NO_KERNEL := true
+# Just defining it, to avoid unncessary error while building...
 BOARD_KERNEL_IMAGE_NAME := Image
+
+# Kernel - source
 TARGET_KERNEL_CONFIG := CK8n_defconfig
 TARGET_KERNEL_SOURCE := kernel/tecno/CK8n
 
@@ -102,15 +110,20 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 # Define vendor_boot image size, as we will be moving recovery resource to it
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := $(BOARD_BOOTIMAGE_PARTITION_SIZE)
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := erofs
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
+# TODO: Fix hardcoded value
+BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := tecno_dynamic_partitions
 BOARD_TECNO_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product system_ext
-BOARD_TECNO_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
+# TODO: Fix hardcoded value
+BOARD_TECNO_DYNAMIC_PARTITIONS_SIZE := 9122611200
+
+# Partitions - file type
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Platform
 TARGET_BOARD_PLATFORM := mt6893
@@ -121,12 +134,19 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
 TARGET_USES_MKE2FS := true
 
 # Recovery - Ramdisk
 # Since moving recovery resources to vendor_boot, ramdisk compression type became lz4-l from gzip
 BOARD_RAMDISK_USE_LZ4 := true
+
+# Recovery - sdcard ?
+RECOVERY_SDCARD_ON_DATA := true
+
+# SAR
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+BOARD_SUPPRESS_SECURE_ERASE := true
 
 # SPL
 PLATFORM_SECURITY_PATCH := 2099-12-31
@@ -149,7 +169,6 @@ TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
-RECOVERY_SDCARD_ON_DATA := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
@@ -162,13 +181,18 @@ TW_EXCLUDE_APEX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_HAS_MTP := true
 TW_HAS_NO_RECOVERY_PARTITION := true
-TW_SKIP_ADDITIONAL_FSTAB := true # We are skipping additional fstab search/generation as we already have enough file-system table define in the tree
 
 # Vendor Boot
 # Making sure recovery build don't have kernel in it--well, no kernel at all in tree anyway... LOL!
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 # We are now moving to vendor_boot, hmmm, wait, really?
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+
+# Workaround for copy_out error
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_SYSTEM_DLKM := system_dlkm
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 
 # Version
 TW_DEVICE_VERSION := A14-CD_0001
